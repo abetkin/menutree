f2ff400f9af1bc1e2bf630fca9398aaf4ba5101f
Vitalii Abetkin <abvit89@gmail.com>
2018-02-06 19:20:04 +0300

    dev setup

--- menu/models.py
+++ menu/models.py
@@ -1,29 +1,54 @@
+import itertools
+from math import ceil, log10
+
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-MAX_CHILDREN_ITEMS = 100
+
 
 #TODO help_text
 
 class MenuItem(models.Model):
+    MAX_CHILDREN_ITEMS = 100
+    _order_digits = ceil(log10(MAX_CHILDREN_ITEMS))
+
     title = models.CharField(max_length=50)
     parent = models.ForeignKey('MenuItem', on_delete=models.CASCADE, null=True, blank=True)
     tooltip = models.CharField(max_length=200, null=True, blank=True)
-    order = models.IntegerField(help_text="Order relative to its parent")
+    order = models.DecimalField(
+        max_digits=_order_digits, decimal_places=0,
+        help_text="Order relative to its parent",
+    )
+
+    
 
     def __str__(self):
         return self.title
     
+    def get_global_order(self):
+        obj = self.parent
+        for i in itertools.count():
+            if obj is None:
+                break
+            obj = obj.parent
+        offset = self.MAX_CHILDREN_ITEMS ** i + self.order
+        return offset + self.order
+
     @classmethod
-    def post_save(cls, sender, **kw):
-        import ipdb; ipdb.set_trace()
+    def post_save(cls, sender, instance=None, **kw):
+        OrderedItem(menuitem_id=instance.id, global_order=instance.get_global_order())
 
 class OrderedItem(MenuItem):
     '''
     Global ordering for items
     '''
-    order = models.IntegerField()
+    MAX_NESTING = 20
+    
+    global_order = models.DecimalField(
+        max_digits=MenuItem._order_digits * MAX_NESTING,
+        decimal_places=0,
+    )
 
 
 receiver(post_save, sender=MenuItem)(MenuItem.post_save)
