ff6a106d0c5d2b7e152fa7498fa98eb3b648a8de
Vitalii Abetkin <abvit89@gmail.com>
2018-02-07 20:11:24 +0300

    working

--- menu/admin.py
+++ menu/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
+from django import forms
+from django.forms import formsets
 
 # Register your models here.
 
 from .models import *
-from .threadlocal import fire_menu_save_started
 
 class MenuItems(admin.TabularInline):
     model = MenuItem
@@ -11,27 +12,42 @@ class MenuItems(admin.TabularInline):
     def get_formset(self, request, obj=None, **kwargs):
         kwargs.update(
             fields=[
-                'title', 'tooltip', 'id',
+                'title', 'tooltip', 'order',
             ]
         )
-        return super().get_formset(request, **kwargs)
+        fs = super().get_formset(request, **kwargs)
+        class MenuItemsFormSet(fs):
+            
+            def save(self, commit=False):
+                objects = super().save(commit=False)
+                existing = [
+                    o.pk for o in objects if o.pk is not None
+                ]
+                self.model.objects.filter(pk__in=existing).delete()
+                orders_list = [o.order for o in objects]
+                if not all(o is not None for o in orders_list):
+                    orders_list = range(1, len(objects) + 1)
+                for obj, order in zip(objects, orders_list):
+                    obj.pk = None
+                    obj.order = order
+                    obj.save()
+                return objects
+
+        return MenuItemsFormSet
+
+
+def global_id(obj):
+    return str(obj.globalid.global_id).lstrip('0.').strip('0')
+global_id.short_description = 'Global Id'
 
 @admin.register(MenuItem)
 class MenuItemAdmin(admin.ModelAdmin):
     inlines = [
         MenuItems
     ]
+    fields = [
+        'title', 'parent', 'tooltip', 'order'
+    ]
 
-    list_display = ['global_order', 'title']
-
-    def _create_formsets(self, *args, **kw):
-        #
-        fire_menu_save_started()
-        return super()._create_formsets(*args, **kw)
-
-    ordering = ['global_order']
-
-    # def get_queryset(self, request):
-    #     qs = super().get_queryset(request)
-    #     return qs.order_by('menuorder__global_order')
-
+    list_display = [global_id, 'title']
+    ordering = ['globalid__global_id']
--- menu/models.py
+++ menu/models.py
@@ -5,27 +5,39 @@ from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from .threadlocal import get_menu_item_order
 
-#TODO help_text
+class GlobalId(models.Model):
+    MAX_NESTING = 20
+    ORDER_DIGITS = 2
+
+    global_id = models.DecimalField(
+        max_digits=ORDER_DIGITS * MAX_NESTING + 2,
+        decimal_places=ORDER_DIGITS * MAX_NESTING,
+    )
+    menu_item = models.OneToOneField('MenuItem', on_delete=models.CASCADE)
+
+    @classmethod
+    def get_global_id(cls, menu_item):
+        orders_list = []
+        obj = menu_item
+        for i in itertools.count():
+            orders_list.append(obj.order)
+            obj = obj.parent
+            if obj is None:
+                break
+        orders_list = [
+            '{:02.0f}'.format(v) for v in reversed(orders_list)
+        ]
+        return ''.join(['0.'] + orders_list)
+
 
 class MenuItem(models.Model):
-    MAX_NESTING = 20
-    MAX_CHILDREN_ITEMS = 100
-    _order_digits = ceil(log10(MAX_CHILDREN_ITEMS))
-
     title = models.CharField(max_length=50)
     parent = models.ForeignKey('MenuItem', on_delete=models.CASCADE, null=True, blank=True)
     tooltip = models.CharField(max_length=200, null=True, blank=True)
-    order = models.DecimalField(
-        max_digits=_order_digits, decimal_places=0,
+    order = models.IntegerField(
         null=True, blank=True,
         help_text="Order relative to its parent",
-    )
-    global_order = models.DecimalField(
-        max_digits=_order_digits * MAX_NESTING,
-        decimal_places=0,
-        null=True, blank=True,
     )
 
     def __str__(self):
@@ -33,19 +45,21 @@ class MenuItem(models.Model):
 
     def save(self, **kw):
         if self.order is None:
-            self.set_order()
+            self.order = self.get_default_order()
         super().save(**kw)
 
-    def set_order(self):
-        self.order = get_menu_item_order(self)
-        self.global_order = self._get_global_order(self.order) \
-            if self.order is not None else None
+    def get_default_order(self):
+        return self.__class__.objects.filter(parent__isnull=True).count() + 1
 
-    def _get_global_order(self, order):
-        obj = self.parent
-        for i in itertools.count():
-            if obj is None:
-                break
-            obj = obj.parent
-        return self.MAX_CHILDREN_ITEMS ** i + order
 
+
+@receiver(post_save, sender=MenuItem)
+def post_save(sender, instance=None, **kwargs):
+    global_id = GlobalId.get_global_id(instance)
+    obj, created = GlobalId.objects.get_or_create(
+        defaults={'global_id': global_id},
+        menu_item=instance,
+    )
+    if not created:
+        obj.global_id = global_id
+    obj.save()
